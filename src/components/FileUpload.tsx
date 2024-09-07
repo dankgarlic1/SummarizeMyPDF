99e92842a233d975d25604ce8d7ebdfae8ec1e2f
Hashit <raizadaharshit2004@gmail.com>
2024-09-07 17:48:04 +0530

    yes

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -4,7 +4,13 @@ import React from "react";
 import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
-  const { getInputProps, getRootProps } = useDropzone();
+  const { getInputProps, getRootProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles);
+    },
+  });
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
