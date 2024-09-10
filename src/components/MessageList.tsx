ef4c463e3089462a41732a7bb655451f69dcfb95
Hashit <raizadaharshit2004@gmail.com>
2024-09-10 19:35:45 +0530

    okk

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { memo } from "react";
 import { Message } from "ai/react";
 import { cn } from "@/lib/utils";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+  return [message]; // If no code blocks, return the entire message as a single block
+}
+
+function isCodeBlock(str: string) {
+  return (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("=>") ||
+    str.includes("<>") ||
+    str.includes("[") ||
+    str.includes("//") ||
+    str.includes("|")
+  );
+}
 type Props = {
   messages: Message[];
 };
 
-const MessageList = ({ messages }: Props) => {
+const MessageList = memo(({ messages }: Props) => {
   if (!messages) {
     return <></>;
   }
   return (
     <div className="flex flex-col gap-2 px-4 ">
       {messages.map((message) => {
+        const messageBlocks = extractCodeFromString(message.content);
         return (
           <div
             key={message.id}
@@ -30,13 +52,25 @@ const MessageList = ({ messages }: Props) => {
                 }
               )}
             >
-              <p>{message.content}</p>
+              {/* Render message blocks */}
+              {messageBlocks.map((block, index) =>
+                isCodeBlock(block) ? (
+                  <SyntaxHighlighter
+                    key={index}
+                    style={materialLight}
+                    language="typescript" // You can set this dynamically based on content
+                  >
+                    {block}
+                  </SyntaxHighlighter>
+                ) : (
+                  <p key={index}>{block}</p>
+                )
+              )}
             </div>
           </div>
         );
       })}
     </div>
   );
-};
-
+});
 export default MessageList;
