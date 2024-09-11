df175e45df079cc4350c87d08291f6e3f1a31369
Hashit <raizadaharshit2004@gmail.com>
2024-09-11 18:12:13 +0530

    ok

---
+++ src/lib/context.ts
@@ -0,0 +1,35 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  fileKey: string
+) {
+  //search through top 5 matching embeddinsg from pinecone
+  const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+  const index = pinecone.Index("summarize-my-pdf");
+  try {
+    const namespace = index.namespace(convertToAscii(fileKey));
+    const queryResult = await namespace.query({
+      topK: 5,
+      includeMetadata: true,
+      vector: embeddings,
+    });
+
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("Error querying embedding", error);
+    throw error;
+  }
+}
+
+export async function getContext(query: string, fileKey: string) {
+  const queryEmbeddings = await getEmbeddings(query);
+  const matches = getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+  const qualifyingDocs = (await matches).filter(
+    (match) => match.score && match.score > 0.7
+  );
+}
